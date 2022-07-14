04b522f2ffad62135c565a66aef17b9f8e50bdfd
VladislavGV <0377722@mail.ru>
2022-07-14 13:14:27 +0300

    Explained AsyncTask class structure

---
+++ app/src/main/java/ru/synergy/asyncexample/AsyncTaskExample.java
@@ -0,0 +1,53 @@
+package ru.synergy.asyncexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AsyncTaskExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async_task_example);
+
+        MyAsyncTask asynctask = new MyAsyncTask();
+        asynctask.execute("Hello World");
+    }
+
+}
+
+class MyAsyncTask extends AsyncTask<String, Integer, Integer> {
+
+
+
+    @Override
+    protected Integer doInBackground(String... strings) {
+        int myProgress = 0;
+        publishProgress(myProgress); // для передачи промежуточного результата
+        int result = myProgress++;
+        return result;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute(); // можно удалить
+    }
+
+    @Override
+    protected void onCancelled() {
+        super.onCancelled(); // можно удалить
+    }
+
+    @Override
+    protected void onPostExecute(Integer integer) {
+        super.onPostExecute(integer);
+    }
+}
---
+++ app/src/main/java/ru/synergy/asyncexample/MainActivity.java
@@ -0,0 +1,25 @@
+package ru.synergy.asyncexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View view) {
+        switch(view.getId()){
+            case R.id.asynctask:
+                Intent intent = new Intent(this, AsyncTaskExample.class);
+                startActivity(intent);
+        }
+    }
+
+}

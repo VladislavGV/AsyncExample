21ad4997e2cc9b031c1263b5d3c9b42c56e587e9
VladislavGV <0377722@mail.ru>
2022-07-19 10:40:20 +0300

    Started threadExample

--- app/src/main/java/ru/synergy/asyncexample/MainActivity.java
+++ app/src/main/java/ru/synergy/asyncexample/MainActivity.java
@@ -15,10 +15,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonClick(View view) {
+        Intent intent = null;
         switch(view.getId()){
             case R.id.asynctask:
-                Intent intent = new Intent(this, AsyncTaskExample.class);
+                 intent = new Intent(this, AsyncTaskExample.class);
                 startActivity(intent);
+            case R.id.thread:
+                intent = new Intent(this, ThreadExample.class);
+                startActivity(intent);
+                break;
+                default: return;
         }
     }
 
